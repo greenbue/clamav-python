13e56306dd70b980dcec59dc928c8a7cc9c4123e
Ryan Ebue <generyan.ebue@gmail.com>
2023-07-05 13:10:27 +1200

    cleanup print statements

--- file_scanner.py
+++ file_scanner.py
@@ -11,13 +11,13 @@ def scan_file(file_path):
       scan_result = cd.scan_file(file_path)
 
       if scan_result == None:
-        print file_path + " is clean."
+        print(file_path + " is clean.")
       else:
-        print file_path + " is infected with " + scan_result[file_path][1] + ". " + scan_result[file_path][0]
+        print(file_path + " is infected with " + scan_result[file_path][1] + ". " + scan_result[file_path][0])
     else:
-      print "File not found."
+      print("File not found.")
   except Exception as e:
-    print "Error during Clam Scan:", str(e)
+    print("Error during Clam Scan:", str(e))
 
 def scan_stream(file_stream):
   try:
@@ -38,25 +38,25 @@ def scan_stream(file_stream):
     else:
       return 'Failed to connect to ClamAV server'
   except pyclamd.ConnectionError as e:
-    print "Error during Clam Scan:", str(e)
+    print("Error during Clam Scan:", str(e))
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
-print "\nScanning file from directory"
+print("\nScanning file from directory")
 filename = os.path.join(current_directory, 'files', 'textfile.txt')
 scan_file(filename)
 
-print "\nScanning file Stream"
+print("\nScanning file Stream")
 with open(filename, 'rb') as file:
   scan_result = scan_stream(file)
 
 print('Scan result for {}: {}'.format(filename, scan_result))
 
-print "\nScanning file from directory"
+print("\nScanning file from directory")
 filename = os.path.join(current_directory, 'files', 'eicar_test.txt')
 scan_file(filename)
 
-print "\nScanning file Stream"
+print("\nScanning file Stream")
 with open(filename, 'rb') as file:
   scan_result = scan_stream(file)
 
